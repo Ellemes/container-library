b8a2329b22d74e9ff70b7bfffa954624bf61e068
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2021-09-09 08:45:42 +0100

    Fix page slot blanking.

--- common/src/main/java/ninjaphenix/container_library/client/gui/PageScreen.java
+++ common/src/main/java/ninjaphenix/container_library/client/gui/PageScreen.java
@@ -54,7 +54,7 @@ public final class PageScreen extends AbstractScreen {
         int slotsPerPage = menuWidth * menuHeight;
         pages = Mth.ceil((double) totalSlots / slotsPerPage);
 
-        blankSlots = slotsPerPage > totalSlots ? slotsPerPage - totalSlots : totalSlots % slotsPerPage;
+        blankSlots = slotsPerPage > totalSlots ? slotsPerPage - totalSlots : slotsPerPage - (totalSlots % slotsPerPage);
 
         imageWidth = 14 + 18 * menuWidth;
         imageHeight = 17 + 97 + 18 * menuHeight;
