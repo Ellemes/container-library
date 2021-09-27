8ad206902490d6ae1ab39b9967cb79533f2910fc
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2021-09-27 13:49:56 +0100

    Remove V1 api.

--- common/forgeSrc/main/java/ninjaphenix/container_library/wrappers/NetworkWrapper.java
+++ common/forgeSrc/main/java/ninjaphenix/container_library/wrappers/NetworkWrapper.java
@@ -1,85 +1,26 @@
 package ninjaphenix.container_library.wrappers;
 
-import ninjaphenix.container_library.Utils;
-import ninjaphenix.container_library.api.OpenableBlockEntity;
-import ninjaphenix.container_library.api.OpenableBlockEntityProvider;
-import ninjaphenix.container_library.api.client.gui.AbstractScreen;
 import ninjaphenix.container_library.api.inventory.AbstractHandler;
 import ninjaphenix.container_library.api.v2.OpenableBlockEntityV2;
-import ninjaphenix.container_library.client.gui.PickScreen;
 import ninjaphenix.container_library.inventory.ServerScreenHandlerFactory;
 import java.util.function.Consumer;
-import net.minecraft.ChatFormatting;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.Container;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 
 public abstract class NetworkWrapper {
     private static NetworkWrapper INSTANCE;
-    private static Client CLIENT_INSTANCE;
-
-    public abstract void initialise();
 
     protected abstract void openScreenHandler(ServerPlayer player, Container inventory, ServerScreenHandlerFactory factory, Component title);
-
-    protected abstract boolean checkUsagePermission(ServerPlayer player, BlockPos pos);
 
     public static NetworkWrapper getInstance() {
         if (NetworkWrapper.INSTANCE == null) {
             NetworkWrapper.INSTANCE = new NetworkWrapperImpl();
         }
         return NetworkWrapper.INSTANCE;
-    }
-
-    private static Client getClientClassInternal() {
-        if (NetworkWrapper.CLIENT_INSTANCE == null) {
-            NetworkWrapper.CLIENT_INSTANCE = new NetworkWrapperImpl.Client();
-        }
-        return NetworkWrapper.CLIENT_INSTANCE;
-    }
-
-    public final void c_openInventoryAt(BlockPos pos) {
-        NetworkWrapper.getClientClassInternal().openInventoryAt(pos);
-    }
-
-    protected final void openScreenHandlerIfAllowed(BlockPos pos, ServerPlayer player) {
-        ServerLevel world = player.getLevel();
-        BlockState state = world.getBlockState(pos);
-        if (state.getBlock() instanceof OpenableBlockEntityProvider block) {
-            OpenableBlockEntity inventory = block.getOpenableBlockEntity(world, state, pos);
-            if (inventory != null) {
-                Component title = inventory.getInventoryTitle();
-                if (inventory.canBeUsedBy(player)) {
-                    if (this.checkUsagePermission(player, pos)) {
-                        block.onInitialOpen(player);
-                    } else {
-                        return;
-                    }
-                } else {
-                    player.displayClientMessage(new TranslatableComponent("container.isLocked", title), true);
-                    player.playNotifySound(SoundEvents.CHEST_LOCKED, SoundSource.BLOCKS, 1.0F, 1.0F);
-                    return;
-                }
-                if (!inventory.canContinueUse(player)) {
-                    return;
-                }
-                this.openScreenHandler(player, inventory.getInventory(), AbstractHandler::new, title);
-            }
-        }
-    }
-
-    public final NetworkWrapperImpl toInternal() {
-        return (NetworkWrapperImpl) this;
     }
 
     public final void s_openInventory(ServerPlayer player, OpenableBlockEntityV2 inventory, Consumer<ServerPlayer> onInitialOpen) {
@@ -91,31 +32,5 @@ public abstract class NetworkWrapper {
         }
         onInitialOpen.accept(player);
         this.openScreenHandler(player, inventory.getInventory(), AbstractHandler::new, title);
-    }
-
-    protected static abstract class Client {
-        abstract void sendOpenInventoryPacket(BlockPos pos);
-        abstract boolean canSendOpenInventoryPacket();
-
-        public final void openInventoryAt(BlockPos pos) {
-            ResourceLocation preference = ConfigWrapper.getInstance().getPreferredScreenType();
-            if (preference.equals(Utils.UNSET_SCREEN_TYPE) || !AbstractScreen.isScreenTypeDeclared(preference)) {
-                Minecraft.getInstance().setScreen(new PickScreen(() -> this.openInventoryAt(pos)));
-            } else {
-                if (this.canSendOpenInventoryPacket()) {
-                    Player player = Minecraft.getInstance().player;
-                    Level world = player.getCommandSenderWorld();
-                    BlockState state = world.getBlockState(pos);
-                    if (state.getBlock() instanceof OpenableBlockEntityProvider provider) {
-                        int invSize = provider.getOpenableBlockEntity(world, state, pos).getInventory().getContainerSize();
-                        if (AbstractScreen.getScreenSize(preference, invSize, Minecraft.getInstance().getWindow().getGuiScaledWidth(), Minecraft.getInstance().getWindow().getGuiScaledHeight()) != null) {
-                            this.sendOpenInventoryPacket(pos);
-                        } else {
-                            player.displayClientMessage(Utils.translation("generic.ninjaphenix_container_lib.label").withStyle(ChatFormatting.GOLD).append(Utils.translation("chat.ninjaphenix_container_lib.cannot_display_screen", Utils.translation("screen." + preference.getNamespace() + "." + preference.getPath() + "_screen")).withStyle(ChatFormatting.WHITE)), false);
-                        }
-                    }
-                }
-            }
-        }
     }
 }
--- fabric/src/main/java/ninjaphenix/container_library/wrappers/NetworkWrapperImpl.java
+++ fabric/src/main/java/ninjaphenix/container_library/wrappers/NetworkWrapperImpl.java
@@ -1,43 +1,17 @@
 package ninjaphenix.container_library.wrappers;
 
-import draylar.goml.api.ClaimUtils;
-import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.screen.ScreenHandler;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import ninjaphenix.container_library.Utils;
 import ninjaphenix.container_library.inventory.ServerScreenHandlerFactory;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 final class NetworkWrapperImpl extends NetworkWrapper {
-    private static final Identifier OPEN_INVENTORY = Utils.id("open_inventory");
-
-    public void initialise() {
-        // Register Server Receivers
-        ServerPlayConnectionEvents.INIT.register((handler, server) -> ServerPlayNetworking.registerReceiver(handler, NetworkWrapperImpl.OPEN_INVENTORY, this::s_handleOpenInventory));
-    }
-
-    private void s_handleOpenInventory(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buffer, PacketSender sender) {
-        BlockPos pos = buffer.readBlockPos();
-        server.execute(() -> this.openScreenHandlerIfAllowed(pos, player));
-    }
-
     @Override
     protected void openScreenHandler(ServerPlayerEntity player, Inventory inventory, ServerScreenHandlerFactory factory, Text title) {
         player.openHandledScreen(new ExtendedScreenHandlerFactory() {
@@ -57,32 +31,5 @@ final class NetworkWrapperImpl extends NetworkWrapper {
                 return factory.create(syncId, inventory, playerInventory);
             }
         });
-    }
-
-    @Override
-    protected boolean checkUsagePermission(ServerPlayerEntity player, BlockPos pos) {
-        AtomicBoolean canUse = new AtomicBoolean(true);
-        if (FabricLoader.getInstance().isModLoaded("goml")) {
-            ClaimUtils.getClaimsAt(player.getServerWorld(), pos).forEach(claim -> {
-                if (!claim.getValue().hasPermission(player)) {
-                    canUse.set(false);
-                }
-            });
-        }
-        return canUse.get();
-    }
-
-    protected static class Client extends NetworkWrapper.Client {
-        @Override
-        void sendOpenInventoryPacket(BlockPos pos) {
-            PacketByteBuf buffer = new PacketByteBuf(Unpooled.buffer());
-            buffer.writeBlockPos(pos);
-            ClientPlayNetworking.send(NetworkWrapperImpl.OPEN_INVENTORY, buffer);
-        }
-
-        @Override
-        boolean canSendOpenInventoryPacket() {
-            return ClientPlayNetworking.canSend(NetworkWrapperImpl.OPEN_INVENTORY);
-        }
     }
 }
