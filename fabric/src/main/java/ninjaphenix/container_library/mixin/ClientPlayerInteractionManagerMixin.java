01da8e6a3be21d0c97ace6fa2972a9aa0a8feba3
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2021-09-30 19:27:02 +0100

    Forgive me for I have sinned.
    Use mixins even on forge to fix an issue with opening inventories when the pick screen is closed before selecting an option.

---
+++ fabric/src/main/java/ninjaphenix/container_library/mixin/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,53 @@
+package ninjaphenix.container_library.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import ninjaphenix.container_library.api.v2.OpenableBlockEntityProviderV2;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public class ClientPlayerInteractionManagerMixin {
+    @Shadow
+    @Final
+    private ClientPlayNetworkHandler networkHandler;
+
+    @Inject(
+            method = "interactBlock(Lnet/minecraft/client/network/ClientPlayerEntity;Lnet/minecraft/client/world/ClientWorld;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Lnet/minecraft/util/ActionResult;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/block/BlockState;onUse(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Lnet/minecraft/util/ActionResult;",
+                    ordinal = 0
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            cancellable = true
+    )
+    private void ncl_noPacketOnFail(ClientPlayerEntity player, ClientWorld world, Hand hand, BlockHitResult hit,
+                                    CallbackInfoReturnable<ActionResult> cir,
+                                    BlockPos pos, ItemStack __itemStack, boolean __bl2) {
+        BlockState state = world.getBlockState(pos);
+        if (state.getBlock() instanceof OpenableBlockEntityProviderV2) {
+            ActionResult result = state.onUse(world, player, hand, hit);
+            if (result == ActionResult.FAIL) {
+                player.swingHand(hand);
+            } else {
+                this.networkHandler.sendPacket(new PlayerInteractBlockC2SPacket(hand, hit));
+            }
+            cir.setReturnValue(result);
+        }
+    }
+}
