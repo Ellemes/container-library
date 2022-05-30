e7b181e7ba1b67b9d20e46fe5fe8b0afbc13ac2d
Ellemes <3llemes@gmail.com>
2022-05-30 19:30:42 +0100

    Update gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("ellemes.gradle.mod").version("1.0.0-alpha.6")
+    id("ellemes.gradle.mod").version("1.0.0-alpha.9")
     //id("com.modrinth.minotaur").version("1.2.1")
     //id("com.matthewprenger.cursegradle").version("1.4.0")
 }
