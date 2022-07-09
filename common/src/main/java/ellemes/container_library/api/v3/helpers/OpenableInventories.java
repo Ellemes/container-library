a4009c5a4ae513c1049ac248f5b1a60fe96cb3e9
Ellemes <3llemes@gmail.com>
2022-07-09 13:11:10 +0100

    Fix up compile error.

--- common/src/main/java/ellemes/container_library/api/v3/helpers/OpenableInventories.java
+++ common/src/main/java/ellemes/container_library/api/v3/helpers/OpenableInventories.java
@@ -3,7 +3,6 @@ package ellemes.container_library.api.v3.helpers;
 import ellemes.container_library.api.helpers.VariableInventory;
 import ellemes.container_library.api.v3.OpenableInventory;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 
@@ -13,17 +12,15 @@ public class OpenableInventories implements OpenableInventory {
     private final OpenableInventory[] parts;
     private final Container inventory;
     private final Component inventoryTitle;
-    private final ResourceLocation forcedScreenType;
 
-    private OpenableInventories(Component inventoryTitle, ResourceLocation forcedScreenType, OpenableInventory... parts) {
+    private OpenableInventories(Component inventoryTitle, OpenableInventory... parts) {
         this.parts = parts;
         this.inventory = VariableInventory.of(Arrays.stream(parts).map(OpenableInventory::getInventory).toArray(Container[]::new));
         this.inventoryTitle = inventoryTitle;
-        this.forcedScreenType = forcedScreenType;
     }
 
-    public static OpenableInventory of(Component inventoryTitle, ResourceLocation forcedScreenType, OpenableInventory... parts) {
-        return new OpenableInventories(inventoryTitle, forcedScreenType, parts);
+    public static OpenableInventory of(Component inventoryTitle, OpenableInventory... parts) {
+        return new OpenableInventories(inventoryTitle, parts);
     }
 
     @Override
@@ -44,10 +41,5 @@ public class OpenableInventories implements OpenableInventory {
     @Override
     public Component getInventoryTitle() {
         return inventoryTitle;
-    }
-
-    @Override
-    public ResourceLocation getForcedScreenType() {
-        return forcedScreenType;
     }
 }
